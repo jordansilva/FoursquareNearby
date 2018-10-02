f027e35ff22b6fa4f1ceb2becd4dd0321e776e55
Jordan Silva <silva.jordan@gmail.com>
2018-10-02 03:17:11 +0200

    Adding Crashlytics

--- app/src/main/java/com/jordansilva/foursquarenearby/app/App.kt
+++ app/src/main/java/com/jordansilva/foursquarenearby/app/App.kt
@@ -3,7 +3,9 @@ package com.jordansilva.foursquarenearby.app
 import android.app.Application
 import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
+import com.crashlytics.android.Crashlytics
 import com.jordansilva.foursquarenearby.app.di.KoinModule
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
@@ -19,11 +21,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        if (BuildConfig.DEBUG) {
-//            Timber.plant(Timber.DebugTree())
-//        }
-
-        //Fabric.with(this, Crashlytics())
+        Fabric.with(this, Crashlytics())
         startKoin(this, listOf(KoinModule.UseCaseModule,
                 KoinModule.ViewModule,
                 KoinModule.RepositoryModule,
